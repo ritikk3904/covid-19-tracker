e5f8c8a5f1dc609ffcf230d5ad2d1390a695e83d
ritikk3904 <77378148+ritikk3904@users.noreply.github.com>
2021-04-29 19:54:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+    var url="https://api.covid19india.org/data.json"
+    
+    $.getJSON(url,function(data){
+        var total_active,total_recovered,total_deaths,total_confirmed
+        var state=[]
+        var confirmed=[]
+        var recovered=[]
+        var deaths=[]
+        
+        $.each(data.statewise,function(id,obj){
+            state.push(obj.state)
+            confirmed.push(obj.confirmed)
+            recovered.push(obj.recovered)
+            deaths.push(obj.deaths)
+        })
+        
+        state.shift()
+        confirmed.shift()
+        recovered.shift()
+        deaths.shift()
+        
+        
+        total_active=data.statewise[0].active
+        total_confirmed=data.statewise[0].confirmed
+        total_recovered=data.statewise[0].recovered
+        total_deaths=data.statewise[0].deaths
+        
+        $("#active").append(total_active)
+        $("#confirmed").append(total_confirmed)
+        $("#recovered").append(total_recovered)
+        $("#deaths").append(total_deaths)
+        
+        
+        var myChart= document.getElementById("myChart").getContext('2d')
+        
+        var chart=new Chart(myChart,{
+            type: 'line',
+            data:{
+                labels:state,
+                datasets:[
+                    {
+                
+                    
+                        label: "Confirmed cases",
+                        data: confirmed,
+                       
+                        minBarLength:100
+                    },
+                    {
+                        label: "Recovered",
+                        data: recovered,
+                        backgroundColor:"#2ecc71",
+                        minBarLength:100
+                    },
+                    {
+                        label: "Decreased",
+                        data: deaths,
+                        backgroundColor:"#e74c3c",
+                        minBarLength:100  
+                    },
+                ]
+            },
+            options: {}
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ world.js
@@ -0,0 +1,141 @@
+$(document).ready(function(){
+    var url="https://api.covid19api.com/summary"
+    
+    $.getJSON(url,function(data){
+        var total_active,total_recovered,total_deaths,total_confirmed,new_confirmed,new_recovered,new_decreased
+        var Country=[]
+        var TotalConfirmed=[]
+        var TotalRecovered=[]
+        var TotalDeaths=[]
+        
+        $.each(data.Array,function(id,obj){
+            Country.push(obj.Country)
+            TotalConfirmed.push(obj.TotalConfirmed)
+            TotalRecovered.push(obj.TotalRecovered)
+            TotalDeaths.push(obj.TotalDeaths)
+
+        })
+        
+        
+        
+        
+        total_active=data.Global[0].active
+        total_confirmed=data.Array[0].TotalConfirmed
+        total_recovered=data.Array[0].TotalRecovered
+        total_deaths=data.Array[0].TotalDeaths
+        
+        
+        $("#active").append(total_active)
+        $("#confirmed").append(total_confirmed)
+        
+        
+        $("#recovered").append(total_recovered)
+        
+        $("#deaths").append(total_deaths)
+        
+        
+        var myChart=document.getElementById("myChart").getContext('2d')
+        var chart=new Chart(myChart,{
+            type:'line',
+            data:{
+                labels:Country,
+                datasets:[
+                    {
+                        label:"Confirmed Cases",
+                        data: TotalConfirmed,
+                        backgroundColor:"#f1c40f",
+                        minBarLength:100
+                    },
+                    
+                    {
+                        label:"Recovered Cases",
+                        data: TotalRecovered,
+                        backgroundColor:"#f1c40f",
+                        minBarLength:100
+                    },
+                    
+                    {
+                        label:"Death Cases",
+                        data: TotalDeaths,
+                        backgroundColor:"#f1c40f",
+                        minBarLength:100
+                    },
+                    
+                    
+                    
+                    
+                ]
+            },
+            options:{}
+        })
+
+
+
+
+
+
+
+
+
+       
+    })
+})
+
+function fetchworld(){
+    $.get("https://api.covid19api.com/summary",
+         function (data){
+            var tbval=document.getElementById('tbval')
+            for(var i=1;i<(data['Countries'].length);i++){
+                var x=tbval.insertRow()
+                x.insertCell(0)
+                
+                tbval.rows[i].cells[0].innerHTML=data['Countries'][i-1]['Country']
+                tbval.rows[i].cells[0].style.background='#6c757d'
+                tbval.rows[i].cells[0].style.color='#fff'
+                
+                x.insertCell(1)
+                
+                tbval.rows[i].cells[1].innerHTML=data['Countries'][i-1]['TotalConfirmed']
+                tbval.rows[i].cells[1].style.background='#ffc107'
+                tbval.rows[i].cells[1].style.color='black'
+                
+                x.insertCell(2)
+                
+                tbval.rows[i].cells[2].innerHTML=data['Countries'][i-1]['TotalRecovered']
+                tbval.rows[i].cells[2].style.background='lightgreen'
+                tbval.rows[i].cells[2].style.color='black'
+                
+                x.insertCell(3)
+                
+                tbval.rows[i].cells[3].innerHTML=data['Countries'][i-1]['TotalDeaths']
+                tbval.rows[i].cells[3].style.background='red'
+                tbval.rows[i].cells[3].style.color='black'
+                
+                x.insertCell(4)
+                
+                tbval.rows[i].cells[4].innerHTML=data['Countries'][i-1]['NewConfirmed']
+                tbval.rows[i].cells[4].style.background='#17a2b8'
+                tbval.rows[i].cells[4].style.color='black'
+                
+                x.insertCell(5)
+                
+                tbval.rows[i].cells[5].innerHTML=data['Countries'][i-1]['NewDeaths']
+                tbval.rows[i].cells[5].style.background='#e83e8c'
+                tbval.rows[i].cells[5].style.color='black'
+
+                
+                
+                
+                
+            }
+        }
+         
+    )
+}
+
+
+
+
+
+
+
