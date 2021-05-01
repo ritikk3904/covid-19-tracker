5b57c69f2f8ea7a89be532e5e9f21b16f04b9b34
ritikk3904 <77378148+ritikk3904@users.noreply.github.com>
2021-05-01 11:40:37 +0530

    Add files via upload

---
+++ coronatry.js
@@ -0,0 +1,147 @@
+$(document).ready(function(){
+    var url="https://api.covid19india.org/data.json"
+    
+    $.getJSON(url,function(data){
+        var total_active,total_recovered,total_deaths,total_confirmed,new_confirmed,new_recovered,new_decreased
+        var state=[]
+        var confirmed=[]
+        var recovered=[]
+        var deaths=[]
+        
+        $.each(data.statewise,function(id,obj){
+            state.push(obj.state)
+            confirmed.push(obj.confirmed)
+            recovered.push(obj.recovered)
+            deaths.push(obj.deaths)
+
+        })
+        
+        state.shift()
+        confirmed.shift()
+        recovered.shift()
+        deaths.shift()
+        
+        
+        
+        total_active=data.statewise[0].active
+        total_confirmed=data.statewise[0].confirmed
+        total_recovered=data.statewise[0].recovered
+        total_deaths=data.statewise[0].deaths
+        
+        
+        $("#active").append(total_active)
+        $("#confirmed").append(total_confirmed)
+        
+        
+        $("#recovered").append(total_recovered)
+        
+        $("#deaths").append(total_deaths)
+        
+        
+        var myChart=document.getElementById("myChart").getContext('2d')
+        var chart=new Chart(myChart,{
+            type:'bar',
+            data:{
+                labels:state,
+                datasets:[
+                    {
+                        label:"Confirmed Cases",
+                        data: confirmed,
+                        backgroundColor:"#f1c40f",
+                        minBarLength:100
+                    },
+                    
+                    {
+                        label:"Recovered Cases",
+                        data: recovered,
+                        backgroundColor:"lightgreen",
+                        minBarLength:100
+                    },
+                    
+                    {
+                        label:"Death Cases",
+                        data: deaths,
+                        backgroundColor:"red",
+                        minBarLength:100
+                    },
+                    
+                    
+                    
+                    
+                ]
+            },
+            options:{}
+        })
+
+
+
+
+
+
+
+
+
+       
+    })
+})
+
+function fetch(){
+    $.get("https://api.covid19india.org/data.json",
+         function (data){
+            var tbval=document.getElementById('tbval')
+            for(var i=1;i<(data['statewise'].length);i++){
+                var x=tbval.insertRow()
+                x.insertCell(0)
+                
+                tbval.rows[i].cells[0].innerHTML=data['statewise'][i-1]['state']
+                tbval.rows[i].cells[0].style.background='#6c757d'
+                tbval.rows[i].cells[0].style.fontWeight='bold'
+                tbval.rows[i].cells[0].style.color='black'
+                
+                x.insertCell(1)
+                
+                tbval.rows[i].cells[1].innerHTML=data['statewise'][i-1]['confirmed']
+                 /*tbval.rows[i].cells[1].style.background='#6c757d'*/
+                 tbval.rows[i].cells[1].style.fontWeight='bold'
+                tbval.rows[i].cells[1].style.color='#ffc107'
+                
+                x.insertCell(2)
+                
+                tbval.rows[i].cells[2].innerHTML=data['statewise'][i-1]['recovered']
+                tbval.rows[i].cells[2].style.fontWeight='bold'
+                tbval.rows[i].cells[2].style.color='Darkgreen'
+                
+                x.insertCell(3)
+                
+                tbval.rows[i].cells[3].innerHTML=data['statewise'][i-1]['deaths']
+                tbval.rows[i].cells[3].style.fontWeight='bold'
+                tbval.rows[i].cells[3].style.color='red'
+                
+                x.insertCell(4)
+                
+                tbval.rows[i].cells[4].innerHTML=data['statewise'][i-1]['deltaconfirmed']
+                tbval.rows[i].cells[4].style.fontWeight='bold'
+                tbval.rows[i].cells[4].style.color='#17a2b8'
+                
+                x.insertCell(5)
+                
+                tbval.rows[i].cells[5].innerHTML=data['statewise'][i-1]['deltadeaths']
+                tbval.rows[i].cells[5].style.fontWeight='bold'
+                tbval.rows[i].cells[5].style.color='#e83e8c'
+
+                
+                
+                
+                
+            }
+        }
+         
+    )
+}
+
+
+
+
+
+
+
